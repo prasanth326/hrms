3af0839631107c00dc1f51b4c5acec236931df67
Pranitha19G <gandrathpranitha@gmail.com>
2025-09-27 00:06:24 +0530

    iconspage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Login from './Components/Login/Login'
 import Signinpage from './Components/SigninPage/Signinpage'
+import Accesspage from './Components/AccessPage/Accesspage'
 function App() {
 
   return (
     <>
      <div>
-      <Login/>
+      {/* <Login/> */}
+      <Accesspage/>
      </div>
     </>
   )
---
+++ src/Components/AccessPage/Accesspage.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import employeeicon from "../../assets/employees.png";
+import reimbursement from "../../assets/reimbursement.png";
+import compensation2 from "../../assets/compensation2.png";
+import attendance from "../../assets/attendance.png";
+import HrPolicies from "../../assets/Hr-Policies.png";
+import hrdocuments from "../../assets/hr-documents-new.png";
+import performance from "../../assets/performance.png";
+import OrgView from "../../assets/OrgView.png";
+import Style from "./Acesspage.module.css";
+import Flows from "../../assets/Flows.png";
+import calendarcimg from "../../assets/calendarc-img.png";
+import performanceCalender from "../../assets/performance.png";
+const Accesspage = () => {
+  const acessPageObject = [
+    {
+      img: employeeicon,
+      title: "Task Box",
+    },
+    {
+      img: employeeicon,
+      title: "Employees",
+    },
+    {
+      img: reimbursement,
+      title: "Reimbursement",
+    },
+    {
+      img: compensation2,
+      title: "Employees",
+    },
+    {
+      img: attendance,
+      title: "Attendance",
+    },
+    {
+      img: HrPolicies,
+      title: "HR Documents",
+    },
+    {
+      img: hrdocuments,
+      title: "HR Documents",
+    },
+    {
+      img: calendarcimg,
+      title: "HR Documents",
+    },
+    {
+      img: performance,
+      title: "Performance",
+    },
+    {
+      img: OrgView,
+      title: "Org View",
+    },
+    {
+      img: Flows,
+      title: "Flows",
+    },
+    {
+      img: performanceCalender,
+      title: "performance",
+    },
+    {
+      img: OrgView,
+      title: "Org View",
+    },
+    {
+      img: Flows,
+      title: "Flows",
+    },
+  ];
+  return (
+    <div className={Style.container}>
+      <div className={Style.navbar}>
+        <div className={Style.logo}>
+          <img src="logo" alt="logo" />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="search by Employee Name, Designation, Department..."
+          ></input>
+        </div>
+        <div className={Style.profile}></div>
+      </div>
+      <div className={Style.dashboardContainer}>
+        <div className={Style.dashboard}>
+          {acessPageObject.map((card, i) => (
+            <div className={Style.card} key={i}>
+              <div className={Style.imageCard}>
+                <img src={card.img} alt={card.title} />
+              </div>
+              <p>{card.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accesspage;
