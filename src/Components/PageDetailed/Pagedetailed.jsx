8ef67320faa4473138bd2682c81e10ccaa940a5e
Pranitha19G <gandrathpranitha@gmail.com>
2025-09-27 22:36:44 +0530

    pagedetailed

---
+++ src/Components/PageDetailed/Pagedetailed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Pagedetailed() {
+    const {title,id}= useParams();
+  return (
+    <div>
+      
+    </div>
+  )
+}
