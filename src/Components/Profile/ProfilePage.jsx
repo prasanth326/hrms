48ed20c636ade561967c781aeff31948b198eb4f
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-09-30 01:56:25 +0530

    profile

--- src/Components/Profile/ProfilePage.jsx
+++ src/Components/Profile/ProfilePage.jsx
@@ -4,7 +4,113 @@ import styles from "./ProfilePage.module.css"
 const ProfilePage = () => {
   return (
     <div className={styles.Container}>
-      ProfilePAGE
+      <h2>Personal Details</h2>
+      <div className={styles.biographical}>
+        <h3>Biographical</h3>
+        <div className={styles.biographicalContainer}>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>First Name</p>
+            <p className={styles.detailspd}>Pranitha</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Middle Name</p>
+            <p className={styles.detailspd}>Patel</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Last Name</p>
+            <p className={styles.detailspd}>Gandrath</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Gander</p>
+            <p className={styles.detailspd}>Female</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Show Date of Birth Year</p>
+            <p className={styles.detailspd}>2001</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Date Of Birth</p>
+            <p className={styles.detailspd}>19-Oct-2001</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Date of Birth Access</p>
+            <p className={styles.detailspd}>Self</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Marital Status</p>
+            <p className={styles.detailspd}>Single</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Marital Status Since</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Marriage Anniversary Access</p>
+            <p className={styles.detailspd}>Self</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Mobile Access</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.divider}></div>
+      <div className={styles.Contact}>
+        <h3>Contact</h3>
+        <div className={styles.biographicalContainer}>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Persanal Email Id</p>
+            <p className={styles.detailspd}>Pranith@easportsca.com</p>
+          </div>
+          <div className={styles.details} >
+            <p className={styles.detailsp}>Persanal Email Id Access</p>
+            <p className={styles.detailspd}>Self</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Persanal Mobile Access</p>
+            <p className={styles.detailspd}>Self</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Persanal Mobile Number</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Office Mobile Access</p>
+            <p className={styles.detailspd}>Self</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Office Mobile No.</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Facebook ID</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>LinkdIn ID</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div className={styles.divider}></div>
+      <div className={styles.Address}>
+        <h3>Address</h3>
+        <div className={styles.biographicalContainer}>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Address as per the Aadhar Card</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Present Portal Address </p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+          <div className={styles.details}>
+            <p className={styles.detailsp}>Permanent Portal Address</p>
+            <p className={styles.detailspd}>****</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
