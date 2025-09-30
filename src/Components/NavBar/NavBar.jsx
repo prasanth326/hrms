81ea372c297c6e9501aabd30202353623ca6a071
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-01 01:50:33 +0530

    select box

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,16 +5,30 @@ import logoaccess from "../../assets/logoaccess.png";
 
 import Style from "../AccessPage/Acesspage.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 const NavBar = () => {
+    const [showNoti, setShowNoti] = useState(false)
+    const [showPro, setShowPro] = useState(false)
+
+    const handleNoti = () => {
+        setShowNoti((prev) => !prev)
+        setShowPro(false)
+    }
+    
+    const handlePro = () => {
+        setShowPro((prev) => !prev)
+        setShowNoti(false)
+    }
+
     return (
         <div className={Style.container}>
             <div className={Style.navbar}>
                 <div className={Style.navbarChild}>
                     <div className={Style.logo}>
                         <Link to="/accesspage" >
-                        <img src={logoaccess} alt="logo" />
+                            <img src={logoaccess} alt="logo" />
                         </Link>
                     </div>
                     <div className={Style.inputsrch}>
@@ -27,11 +41,31 @@ const NavBar = () => {
                     </div>
                     <div className={Style.profile}>
                         <div className={Style.profileIconclock}><img src={clockin} alt="clockin" /></div>
-                        <div className={Style.profileIconnotifi}><img src={notif} alt="notif" /></div>
-                        <div className={Style.profileicon}><img src={notif} alt="notif" /></div>
+                        <div className={Style.profileIconnotifi} onClick={() => handleNoti()}><img src={notif} alt="notif" /></div>
+                        <div className={Style.profileicon} onClick={() => handlePro()}><img src={notif} alt="notif" /></div>
                     </div>
                 </div>
             </div>
+            {
+                showNoti ? <div className={Style.notification}>
+                    <div className={Style.notificat}>
+                         <p>Notification 1</p>
+                         <p>Notification 2</p>
+                         <p>Notification 3</p>
+                         <p>Notification 4</p>
+                         <p>Notification 5</p>
+                    </div>
+                </div> : null
+            }
+            {
+                showPro ? <div className={Style.profileNoti}>
+                    <div className={Style.notificat}>
+                         <p>Profile</p>
+                         <p>Profile</p>
+                         <p>Profile</p>
+                    </div>
+                </div> : null
+            }
         </div>
     );
 };
