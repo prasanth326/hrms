433af9ad9eec0ef8b47b38d4741b290de7b84377
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-02 01:53:49 +0530

    attendance

--- src/Components/Leave/MyLeavePattern/MyLeavePattern.jsx
+++ src/Components/Leave/MyLeavePattern/MyLeavePattern.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "./MyLeavePattern.module.css"
+import chat from "../../../assets/chart.png"
 
 export default function MyLeavePattern() {
     return (
@@ -7,7 +8,10 @@ export default function MyLeavePattern() {
             <div className={styles.leaveBCotainer1}>My Leave Pattern</div>
             <div className={styles.diver}></div>
             <div className={styles.leaveBCotainer2}>
-                <div>Leave Utilized</div>
+                <div className={styles.leaveBCotainerchat}>
+                    <div className={styles.leaveBCotainerchatlive}>Leave Utilized : </div>
+                    <div  className={styles.leaveBCotainerchatliveimg}><img src={chat}/> 25.5</div>
+                </div>
                 <div> &#136;</div>
             </div>
         </div>
