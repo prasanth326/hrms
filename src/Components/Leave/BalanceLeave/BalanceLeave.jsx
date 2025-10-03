8cf88fcf270a5b89efcc315f62514ed50f5c8622
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-03 22:45:59 +0530

    leave balance

--- src/Components/Leave/BalanceLeave/BalanceLeave.jsx
+++ src/Components/Leave/BalanceLeave/BalanceLeave.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./BalanceLeave.module.css"
 import unpaidLeave from "../../../assets/unpaidLeave.png"
 
-export default function BalanceLeave() {
+export default function BalanceLeave({ setleavesTabClose, setLeaveDetailsWindow }) {
+  const [open, setOpen] = useState("")
+  const [openB, setOpenB] = useState(false)
+
+  const openBox = (leave) => {
+    setOpen(leave)
+    setOpenB((prev) => !prev)
+  }
+
+  const openTabDetails = () => {
+    setleavesTabClose(true)
+    setLeaveDetailsWindow(open)
+    setOpenB(false)
+  }
   return (
     <div className={styles.leaveCotainer}>
       <div className={styles.leaveCotaineraBal}>Balance as of Today</div>
       <div className={styles.leaveBalanceCotainer}>
         <div className={styles.leaveBalanceBox}>
           <div className={styles.leave1Compdiv}>
-           <div className={styles.colorr}></div>
+            <div className={styles.colorr}></div>
             <div className={styles.leave1Comp}>
               <span className={styles.leaveAddOne}>1</span>
               <span className={styles.leaveAddComp}>Comp off</span>
             </div>
             <div className={styles.leaveAdd}>
               <span className={styles.leaveAddDotAdd}>+</span>
-              <div className={styles.leaveAddDot}><p>...</p></div>
+              <div className={styles.leaveAddDot} onClick={() => { openBox("compOff") }}><p>...</p></div>
             </div>
+            {open === "compOff" && openB && <div className={styles.LeaveDetails}>
+              <div onClick={() => openTabDetails()}>Leave Details</div>
+              <div>Comp off credit list</div>
+            </div>
+            }
           </div>
           <div className={styles.diver}></div>
           <div className={styles.leaveAdd2}></div>
         </div>
         <div className={styles.leaveBalanceBox}>
           <div className={styles.leave1Compdiv}>
-                       <div className={styles.colorr2}></div>
+            <div className={styles.colorr2}></div>
 
             <div className={styles.leave1Comp}>
               <span className={styles.leaveAddOne}>6</span>
@@ -32,8 +50,14 @@ export default function BalanceLeave() {
             </div>
             <div className={styles.leaveAdd}>
               <span className={styles.leaveAddDotAdd}>+</span>
-              <div className={styles.leaveAddDot}><p>...</p></div>
+              <div className={styles.leaveAddDot} onClick={() => { openBox("Casual Leaves") }}><p>...</p></div>
             </div>
+            {open === "Casual Leaves" && openB && <div className={styles.LeaveDetails}>
+              <div onClick={() => openTabDetails()}>Leave Details</div>
+
+              <div>Casual Leaves credit list</div>
+            </div>
+            }
           </div>
           <div className={styles.diver}></div>
           <div className={styles.leaveAdd2}>
@@ -50,8 +74,14 @@ export default function BalanceLeave() {
             </div>
             <div className={styles.leaveAdd}>
               <span className={styles.leaveAddDotAdd}>+</span>
-              <div className={styles.leaveAddDot}><p>...</p></div>
+              <div className={styles.leaveAddDot} onClick={() => { openBox("Earned Leaves") }}><p>...</p></div>
             </div>
+            {open === "Earned Leaves" && openB && <div className={styles.LeaveDetails}>
+              <div onClick={() => openTabDetails()}>Leave Details</div>
+
+              <div>Earned Leaves credit list</div>
+            </div>
+            }
           </div>
           <div className={styles.diver}></div>
           <div className={styles.leaveAdd2}>
@@ -61,7 +91,7 @@ export default function BalanceLeave() {
         </div>
         <div className={styles.leaveBalanceBox}>
           <div className={styles.leave1Compdiv}>
-                       <div className={styles.colorr4}></div>
+            <div className={styles.colorr4}></div>
 
             <div className={styles.leave1Comp}>
               <span className={styles.leaveAddOne}>5</span>
@@ -69,8 +99,14 @@ export default function BalanceLeave() {
             </div>
             <div className={styles.leaveAdd}>
               <span className={styles.leaveAddDotAdd}>+</span>
-              <div className={styles.leaveAddDot}><p>...</p></div>
+              <div className={styles.leaveAddDot} onClick={() => { openBox("Sick Leaves") }}><p>...</p></div>
             </div>
+            {open === "Sick Leaves" && openB && <div className={styles.LeaveDetails}>
+              <div onClick={() => openTabDetails()}>Leave Details</div>
+
+              <div>Sick Leaves credit list</div>
+            </div>
+            }
           </div>
           <div className={styles.diver}></div>
           <div className={styles.leaveAdd2}>
@@ -83,14 +119,20 @@ export default function BalanceLeave() {
             <div className={styles.colorr5}></div>
             <div className={styles.leave1Comp}>
               <span className={styles.leaveAddOne}>
-                <img src={unpaidLeave} alt='unpaidLeave'/>
+                <img src={unpaidLeave} alt='unpaidLeave' />
               </span>
               <span className={styles.leaveAddComp}>Loss of Pay</span>
             </div>
             <div className={styles.leaveAdd}>
               <span className={styles.leaveAddDotAdd}>+</span>
-              <div className={styles.leaveAddDot}><p>...</p></div>
+              <div className={styles.leaveAddDot} onClick={() => { openBox("Loss of Pay") }}><p>...</p></div>
             </div>
+            {open === "Loss of Pay" && openB && <div className={styles.LeaveDetails}>
+              <div onClick={() => openTabDetails()}>Leave Details</div>
+
+              <div>Loss of Pay credit list</div>
+            </div>
+            }
           </div>
           <div className={styles.diver}></div>
           <div className={styles.leaveAddReady}>
--- src/Components/Leave/LeaveBalance/LeaveBalance.jsx
+++ src/Components/Leave/LeaveBalance/LeaveBalance.jsx
@@ -3,19 +3,62 @@ import BalanceLeave from '../BalanceLeave/BalanceLeave'
 import styles from "./LeaveBalance.module.css"
 import MyLeavePattern from '../MyLeavePattern/MyLeavePattern'
 import LeaveHeaderTab from '../LeaveHeaderTab/LeaveHeaderTab'
+import { Select } from '@mui/material'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 export default function LeaveBalance() {
-  const[leaveTab, setLeaveTab]=useState ("Leave Balance");
+  const [leaveTab, setLeaveTab] = useState("Leave Balance");
+  const [leaveDetailsWindow, setLeaveDetailsWindow] = useState("");
+  const [leaves, setLeaves] = React.useState('Comp');
+  const [leavesTabClose, setleavesTabClose] = React.useState(false);
+
+  const handleChange = (event) => {
+    setLeaves(event.target.value);
+  };
+
+  const leaveWindowsx = () => {
+    setleavesTabClose((prev) => !prev)
+  }
 
   return (
     <div className={styles.leaveBCotainerMain}>
-      <LeaveHeaderTab setLeaveTab={setLeaveTab} leaveTab={leaveTab}/>
+      <LeaveHeaderTab setLeaveTab={setLeaveTab} leaveTab={leaveTab} />
       <div className={styles.leaveBCotainerMaindiv}>
         <MyLeavePattern />
-        {leaveTab === "Leave Balance" && <BalanceLeave />}
+        {leaveTab === "Leave Balance" && <BalanceLeave setLeaveDetailsWindow={setLeaveDetailsWindow} setleavesTabClose={setleavesTabClose} />}
         {leaveTab === "Holiday List" && "Holiday List"}
         {leaveTab === "History" && "History"}
       </div>
+      {leavesTabClose && leaveDetailsWindow && <div className={styles.leaveWindowsOverLay} onClick={(e) => {setleavesTabClose(false)}}>
+        <div className={styles.leaveWindows} onClick={(e) => {e.stopPropagation()}}>
+          <div className={styles.leaveWindowsdetails}>
+            <div>Leave Details</div>
+            <div className={styles.leaveWindowsx} onClick={() => leaveWindowsx()}>x</div>
+          </div>
+          <div className={styles.leaveWindowsdiver}></div>
+          <div className={styles.leaveWindowsdiverBox}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={leaves}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Comp"}>Comp off</MenuItem>
+                  <MenuItem value={"Casual"}>Casual Leaves</MenuItem>
+                  <MenuItem value={"Earned"}>Earned Leaves</MenuItem>
+                  <MenuItem value={"Sick"}>Sick Leaves</MenuItem>
+                  <MenuItem value={"Loss"}>Loss of Pay</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+        </div>
+      </div>}
     </div>
   )
 }
