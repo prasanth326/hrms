ffa1ed89f77113da6bad9d8ce4d57a9eb6c08dfb
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-09 20:41:25 +0530

    s

--- src/Components/Performance/GoalPlan/GoalPlan.jsx
+++ src/Components/Performance/GoalPlan/GoalPlan.jsx
@@ -1,21 +1,70 @@
 import React from "react";
 import styles from "./GoalPlan.module.css";
+import { Button } from "@mui/material";
+import benifits1 from "../../../assets/benifits1.png"
 
 export default function GoalPlan() {
   return (
     <div className={styles.container}>
       <div className={styles.firstcontainer}>
-        <div>GoalPlan</div>
-        <div className={styles.updategoal}>Update Goal Plan</div>
+        <div className={styles.firstcontainerimg}>
+          <div className={styles.firstcontainerimg2}> <img src={benifits1} /></div>
+          <div><p>Goal Plan</p></div>
+        </div>
+        <div><Button variant="contained">Update Goal Plan</Button></div>
       </div>
       <div className={styles.secondOverview}>
         <div className={styles.secondOverviewcard}>
-        <div className={styles.zero}>0</div>
-        <div className={styles.Objectives}>Objectives</div>
+          <div className={styles.zero}><span>0</span></div>
+          <div className={styles.Objectives}>Objectives</div>
         </div>
       </div>
       <div className={styles.ThirdOverview}>
         <p>Goals/keys Result Areas Overview</p>
+        <div className={styles.ThirdOverview2}>
+          <div className={styles.imgbeniand}>            
+            <div className={styles.imgbeni}>
+              <div><img src={benifits1} /></div>
+              <div>0</div>
+            </div>
+            <div>Completed</div>
+          </div>
+          <div className={styles.imgbeniand}>            
+          <div className={styles.imgbeni}>
+            <div><img src={benifits1} /></div>
+            <div>0</div>
+          </div>
+            <div>In Progress</div>
+          </div>
+          <div className={styles.imgbeniand}>            
+          <div className={styles.imgbeni}>
+            <div><img src={benifits1} /></div>
+            <div>0</div>
+          </div>
+            <div>On Hold</div>
+          </div>
+          <div className={styles.imgbeniand}>            
+          <div className={styles.imgbeni}>
+            <div><img src={benifits1} /></div>
+            <div>0</div>
+          </div>
+            <div>On Rsld</div>
+          </div>
+          <div className={styles.imgbeniand}>            
+            <div className={styles.imgbeni}>
+              <div><img src={benifits1} /></div>
+              <div>0</div>
+            </div>
+            <div>Delayed</div>
+          </div>
+          <div className={styles.imgbeniand}>            
+            <div className={styles.imgbeni}>
+              <div><img src={benifits1} /></div>
+              <div>0</div>
+            </div>
+              <div>Not Satled</div>
+          </div>
+        </div>
       </div>
     </div>
   );
