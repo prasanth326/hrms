87aadcb4d1a34af1bcba7b41eb831289c22a799c
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-23 00:08:54 +0530

    s

--- src/Components/Overview/Reimbursement/ReimbursementTab/TagExpenseButton/TagExpenseButton.jsx
+++ src/Components/Overview/Reimbursement/ReimbursementTab/TagExpenseButton/TagExpenseButton.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./TagExpenseButton.module.css";
 import sidebarsearchclose from "../../../../../assets/sidebarsearchclose.png";
+import no_bills from "../../../../../assets/no_bills.png"
+
 
 export default function TagExpenseButton() {
   return (
@@ -16,23 +18,34 @@ export default function TagExpenseButton() {
       </div>
       <div className={styles.TagExpensetablediv}>
         <div className={styles.TagExpensetable}>
-            <div>Total 0 Expenses</div>
-            <div className={styles.expensetable}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Expense Type</th>
-                    <th>Merchant</th>
-                    <th>Expense Type</th>
-                    <th>Expense Type</th>
-                    <th>Expense Type</th>
-                    <th>Expense Type</th>
-                  </tr>
-                </thead>
-              </table>
+          <div>Total 0 Expenses</div>
+          <div className={styles.expensetable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Expense Type</th>
+                  <th>Merchant</th>
+                  <th>Expense Type</th>
+                  <th>Expense Type</th>
+                  <th>Expense Type</th>
+                  <th>Expense Type</th>
+                </tr>
+              </thead>
+            </table>
           </div>
         </div>
-        <div>box</div>
+        <div>
+          <div className={styles.preview}>
+            <div className={styles.previewDiv}>Preview</div>
+            <div className={styles.previewimg}>
+              <img src={no_bills} />
+              <p>No expense selected</p>
+              <p>Open any expense and</p>
+              <p>preview will be loaded with</p>
+              <p>attachments automatically</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
