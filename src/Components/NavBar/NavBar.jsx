8f6f9e7e4148c324603d9be237c1e65ad4809f20
prasanth balaga <prasanthbalaga@prasanths-MacBook-Air.local>
2025-10-31 23:49:31 +0530

    s

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,12 +3,13 @@ import notif from "../../assets/notif.png";
 import clockin from "../../assets/clockin.png";
 import logoaccess from "../../assets/logoaccess.png";
 import menu from "../../assets/menu.png";
+import more from "../../assets/more.png";
 
 import { useNavigate } from "react-router-dom";
 
 import Style from "../AccessPage/Acesspage.module.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 
 import styles from "./NavBar.module.css";
@@ -17,8 +18,10 @@ import DashBoardSideBar from "../DashBoardSideBar/DashBoardSideBar";
 const NavBar = ({ qrlocation }) => {
   const [showNoti, setShowNoti] = useState(false);
   const [showPro, setShowPro] = useState(false);
-  const [showDashboard, setShowDashBoard] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
+  const [showDashboard, setShowDashBoard] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth)
 
   const navigate = useNavigate()
 
@@ -37,19 +40,32 @@ const NavBar = ({ qrlocation }) => {
     setShowPro(false)
   }
 
+  const onclickMenu = () => {
+    setShowMenu((prev) => !prev)
+  }
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  useEffect(() => {
+    if (width >= 720) {
+      setShowMenu(false)
+    }
+  }, [width])
   return (
     <div className={Style.container}>
       <div className={Style.navbar}>
         <div className={Style.navbarChild}>
           <div className={Style.logo}>
             {
-              qrlocation ? 
-                <img src={menu} alt="menu" onClick={()=>{setShowDashBoard(true)}}/> : 
+              qrlocation ?
+                <img src={menu} alt="menu" onClick={() => { setShowDashBoard(true) }} /> :
                 <Link to="/accesspage">
                   <img src={logoaccess} alt="logo" />
                 </Link>
             }
-
           </div>
           <div className={Style.inputsrch}>
             <input
@@ -60,18 +76,21 @@ const NavBar = ({ qrlocation }) => {
               <img src={search} alt="search" />
             </div>
           </div>
-          <div className={Style.profile}>
-            <div className={Style.profileIconclock}>
-              <img src={clockin} alt="clockin" />
-            </div>
-            <div
-              className={Style.profileIconnotifi}
-              onClick={() => handleNoti()}
-            >
-              <img src={notif} alt="notif" />
-            </div>
-            <div className={Style.profileicon} onClick={() => handlePro()}>
-              <img src={notif} alt="notif" />
+          <div className={Style.profilemore2}>
+            <div className={Style.profilemore} onClick={() => onclickMenu()}><img src={more} /></div>
+            <div className={`${showMenu ? Style.profile : Style.profileHide}`}>
+              <div className={Style.profileIconclock}>
+                <img src={clockin} alt="clockin" />
+              </div>
+              <div
+                className={Style.profileIconnotifi}
+                onClick={() => handleNoti()}
+              >
+                <img src={notif} alt="notif" />
+              </div>
+              <div className={Style.profileicon} onClick={() => handlePro()}>
+                <img src={notif} alt="notif" />
+              </div>
             </div>
           </div>
         </div>
@@ -109,7 +128,7 @@ const NavBar = ({ qrlocation }) => {
       ) : null}
 
       {
-        <DashBoardSideBar isOpen={showDashboard}  onClose={() => setShowDashBoard(false)}/>
+        <DashBoardSideBar isOpen={showDashboard} onClose={() => setShowDashBoard(false)} />
       }
     </div>
   );
